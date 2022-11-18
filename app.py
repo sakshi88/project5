94d63d25dfe4482e985171216976edeaf615d10b
sakshi88 <sakshi.kataria12@gmail.com>
2022-11-19 00:27:13 +0530

    Linting corrected

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "<h1 style='color: red;'>Welcome to Sakshi's Final/Capstone Project!!!</h1>"
-abbs
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
