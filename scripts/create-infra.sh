3ddb54ee050aeaa4af0fecd7f45a51a2815711f8
sakshi88 <sakshi.kataria12@gmail.com>
2022-11-18 23:55:18 +0530

    changed kubernetes version

--- scripts/create-infra.sh
+++ scripts/create-infra.sh
@@ -5,6 +5,6 @@ if [[ $status -gt 0 ]]
     echo "Skipping... as Infrastructure Exists"
     else
     echo "starting Creating Kubernetes cluster"
-    eksctl create cluster --name capstone --region $AWS_DEFAULT_REGION --version 1.19  --nodegroup-name capstone-ng --node-type t3.large --nodes-min 2 --nodes-max 2 --managed
+    eksctl create cluster --name capstone --region $AWS_DEFAULT_REGION --version 1.20  --nodegroup-name capstone-ng --node-type t3.large --nodes-min 2 --nodes-max 2 --managed
     echo "cluster created"
 fi
